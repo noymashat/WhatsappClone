aab6c81f8f5b5233e18887674076b21ebf1faf52
noymashat <34368457+noymashat@users.noreply.github.com>
2020-10-21 17:53:37 +0300

    implement api + user model

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import useCachedResources from "./hooks/useCachedResources";
@@ -8,11 +8,48 @@ import Navigation from "./navigation";
 import Amplify from "aws-amplify";
 import config from "./aws-exports";
 
+import { withAuthenticator } from "aws-amplify-react-native";
+import { Auth, API, graphqlOperation } from "aws-amplify";
+import { getUser } from "./graphql/queries";
+import { createUser } from "./graphql/mutations";
+
 Amplify.configure(config);
 
-export default function App() {
+function App() {
 	const isLoadingComplete = useCachedResources();
 	const colorScheme = useColorScheme();
+
+	// run when App is first mounted
+	useEffect(() => {
+		const fetchUser = async () => {
+			//get authenticated user from auth
+			const userInfo = await Auth.currentAuthenticatedUser({
+				bypassCache: true
+			});
+			if (userInfo) {
+				//get the user from backend with  the user SUB from auth
+				const userData = await API.graphql(
+					graphqlOperation(getUser, { id: userInfo.attributes.sub })
+				);
+
+				if (userData.data.getUser) {
+					console.log("User is already registered in database");
+					return;
+				}
+				const newUser = {
+					id: userInfo.attributes.sub,
+					name: userInfo.username,
+					imageUri:
+						"https://hieumobile.com/wp-content/uploads/avatar-among-us-9.jpg",
+					status: "Hey there, I'm using Whatsapp"
+				};
+
+				//if thereis no user in db with th id, then create one
+				await API.graphql(graphqlOperation(createUser, { input: newUser }));
+			}
+		};
+		fetchUser();
+	}, []);
 
 	if (!isLoadingComplete) {
 		return null;
@@ -25,3 +62,5 @@ export default function App() {
 		);
 	}
 }
+
+export default withAuthenticator(App);
